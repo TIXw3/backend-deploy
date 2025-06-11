4b56a1ecafa6a4450e7a7467b09eed244197a631
TIXw3 <atixup@gmail.com>
2025-06-11 09:17:51 -0300

    Update ingressoRoutes.js

--- backend-tixup-main/src/routes/ingressoRoutes.js
+++ backend-tixup-main/src/routes/ingressoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middlewares/authMiddleware");
-const verificaTipoUsuario = require("../middlewares/verificaTipoUsuario");
+const { verificaTipoUsuario } = require("../middlewares/verificaTipoUsuario");
 const ingressosController = require("../controllers/ingressosController");
 
 router.post(
