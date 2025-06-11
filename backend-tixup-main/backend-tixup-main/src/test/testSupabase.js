4a9a83e3b450bd6d3161e7e95ef8fe01482bc255
TIXw3 <atixup@gmail.com>
2025-06-11 10:19:55 -0300

    Delete backend-tixup-main/backend-tixup-main directory

--- backend-tixup-main/backend-tixup-main/src/test/testSupabase.js
+++
@@ -1,9 +0,0 @@
-const supabase = require("./config/supabaseClient");
-
-async function testConnection() {
-  const { data, error } = await supabase.from("usuarios").select("*");
-  if (error) console.error("Erro ao buscar usuários:", error);
-  else console.log("Usuários:", data);
-}
-
-testConnection();
